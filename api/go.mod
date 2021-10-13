086952ece68520790ee8568822a1d4f2ce81216e
renasami <drrr0502@gmail.com>
2021-10-13 21:22:23 +0900

    on the way of main.go

--- api/go.mod
+++ api/go.mod
@@ -10,8 +10,10 @@ require (
 	github.com/fsnotify/fsnotify v1.5.1 // indirect
 	github.com/gin-gonic/gin v1.7.4 // indirect
 	github.com/gofiber/fiber/v2 v2.20.1
+	github.com/jinzhu/gorm v1.9.16 // indirect
 	github.com/klauspost/compress v1.13.6 // indirect
 	github.com/mattn/go-colorable v0.1.11 // indirect
+	github.com/mattn/go-sqlite3 v1.14.8 // indirect
 	github.com/pelletier/go-toml v1.9.4 // indirect
 	github.com/stretchr/testify v1.7.0
 	github.com/valyala/fasthttp v1.31.0 // indirect
--- api/main.go
+++ api/main.go
@@ -2,7 +2,62 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
+	"github.com/jinzhu/gorm"
+
+	_ "github.com/mattn/go-sqlite3"
 )
+
+type Todo struct {
+	gorm.Model
+	Text   string
+	Status string
+}
+
+//initialize db
+func dbInit() {
+	db, err := gorm.Open("sqlite3", "test.sqlite3")
+	if err != nil {
+		panic("we cannot open db at dbInit")
+	}
+	db.AutoMigrate((&Todo{}))
+	defer db.Close()
+}
+
+//add db
+func dbInsert(text string, status string) {
+	db, err := gorm.Open("sqlite3", "test.sqlite3")
+	if err != nil {
+		panic("we cannot open db at dbInsert")
+	}
+	db.Create(&Todo{Text: text, Status: status})
+	defer db.Close()
+}
+
+// update db
+func dbUpdate(id int, text string, status string) {
+	db, err := gorm.Open("sqlite3", "test.sqlite3")
+	if err != nil {
+		panic("we cannot open db at dbUpdate")
+	}
+	var todo Todo
+	db.First(&todo, id)
+	todo.Text = text
+	todo.Status = status
+	db.Save(&todo)
+	db.Close()
+}
+
+//delete db
+func dbDelete(id int) {
+	db, err := gorm.Open("sqlite3", "test.sqlite3")
+	if err != nil {
+		panic("we cannot open db at dbDelete")
+	}
+	var todo Todo
+	db.First(&todo, id)
+	db.Delete(&todo)
+	db.Close()
+}
 
 func main() {
 	router := gin.Default()
